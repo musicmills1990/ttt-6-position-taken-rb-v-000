51dfe0d0bc6115aaa48dce3aa381a6ee337ecb8c
Marc Mills <mdmills1990@gmail.com>
2018-08-01 17:59:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,12 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
   if board[index] == " "
     puts false
+  elsif board[index] == ""
+    puts false
+  elsif board[index] == "X"
+    puts true
+  elsif board[index] == "O"
+    puts true
+  else nil
   end
 end
